b21322b6e7829177a5c0ad37570ef2e7079400c0
renan-77 <renan.deoliveiramonteiro@student.griffith.ie>
2019-04-11 15:34:04 +0100

    JavaAssignment

--- src/com/orenan/assignments/Main.java
+++ src/com/orenan/assignments/Main.java
@@ -1,32 +1,19 @@
 package com.orenan.assignments;
-
 import java.util.Arrays;
-
 public class Main {
-
-
     public static void main(String[] args) {
         int odd = 0;
         int even = 0;
         int[] numbers = {41, 21, 325, 12, 24};
         Arrays.sort(numbers);
-
         System.out.println("The smallest value is: " + numbers[0] + " and the largest one is " + numbers[4] + ".");
-
         for(int i = 0; i < numbers.length; i++){
-
             if(numbers[i] % 2 == 0){
-
                 even++;
-
             }else{
-
                 odd++;
-
             }
-
         }
-
         System.out.println("There are " + even + " EVEN numbers and " + odd + " ODD numbers.");
     }
 }
